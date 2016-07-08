6e1499dc99035e210e761a16f6a7706cfef43c4f
ChristianHemann <christian.hemann@yahoo.de>
2016-07-08 14:02:50 +0200

    edit reset cone position

--- Assets/Scripts/UnityInterface/Cone.cs
+++ Assets/Scripts/UnityInterface/Cone.cs
@@ -8,6 +8,27 @@ namespace UnityInterface
 {
     public class Cone : MonoBehaviour
     {
+        private Vector3 _normalPosition;
+        void Start()
+        {
+            _normalPosition = transform.position;
+        }
 
+        void OnCollisionEnter() //nachschauen ob die function wirklich so heißt
+        {
+            //setze timer und rufe function ResetCone nach x sekunden auf
+        }
+
+        void ResetCone()
+        {
+            transform.position = _normalPosition;
+        }
+
+
+
+        
+        //der code ist zum position des autos setzen; löschen sobald benutzt
+            //this.transform.position = OverallCarOutput.LastCalculation.Position.ToUnityVector3();
+            //this.transform.rotation = Quaternion.Euler(OverallCarOutput.LastCalculation.Direction.ToUnityVector3());
     }
 }
