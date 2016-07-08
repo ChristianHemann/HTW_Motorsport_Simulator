c7e87f8aee725fea9bba65d84767d6f0b1cfc8fc
loic1993 <s0546433@htw-berlin.de>
2016-07-08 13:19:21 +0200

    camera

---
+++ Assets/Scripts/UnityInterface/CameraFollowCar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace UnityInterface
+{
+   public class CameraFollowCar : MonoBehaviour
+    {
+        public GameObject target;
+        public float xOffset = 0;
+        public float yOffset = 0;
+        public float zOffset = 0;
+
+        void LateUpdate()
+        {
+            this.transform.position = new Vector3(target.transform.position.x + xOffset,
+                                                  target.transform.position.y + yOffset,
+                                                  target.transform.position.z + zOffset);
+        }
+    }
+}
