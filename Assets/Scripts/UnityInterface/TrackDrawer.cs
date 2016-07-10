342c1a7048b4bb425f09f521b1f325a4eccc3562
ChristianHemann <christian.hemann@yahoo.de>
2016-07-10 22:20:41 +0200

    edit TrackDrawer
    set car position when started

--- Assets/Scripts/UnityInterface/TrackDrawer.cs
+++ Assets/Scripts/UnityInterface/TrackDrawer.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using CalculationComponents;
-using MathNet.Numerics.LinearAlgebra;
+﻿using CalculationComponents;
+using CalculationComponents.TrackComponents;
+using Output;
 using UnityEngine;
 
 namespace UnityInterface
@@ -11,14 +8,26 @@ namespace UnityInterface
     public class TrackDrawer : MonoBehaviour
     {
         private ImportantClasses.Vector2[] _positions;
-        public GameObject cone;
+        public GameObject Cone;
 
         void Start()
         {
+            //sets the car on the starting line
+            foreach (TrackSegment trackSegment in Track.Instance.TrackSegments)
+            {
+                if (trackSegment is StartLine)
+                {
+                    OverallCarOutput.LastCalculation.Direction = trackSegment.EndDirection;
+                    OverallCarOutput.LastCalculation.Position = trackSegment.EndPoint;
+                    break;
+                }
+            }
+
+            //place cones
             _positions = Track.Instance.GetConePositions();
             foreach (ImportantClasses.Vector2 position in _positions)
             {
-                GameObject.Instantiate(cone, new Vector3(position.X, 0f, position.Y), new Quaternion(0, 0, 0, 0));
+                Instantiate(Cone, new Vector3(position.X, 0f, position.Y), new Quaternion(0, 0, 0, 0));
             }
         }
     }
